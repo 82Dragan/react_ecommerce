196fccc2158305e30796fb86f35fc350ec1a6028
82Dragan <82dragan@gmail.com>
2021-07-24 22:50:35 +0200

    Cart completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 import { Products, Navbar, Cart } from './components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
 const App = () => {
@@ -24,6 +25,24 @@ const App = () => {
         setCart(item.cart)
     }
 
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const {cart} = await commerce.cart.update(productId, { quantity })
+
+        setCart(cart)
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId)
+
+        setCart(cart)
+    }
+
+    const handleEmptyCart = async () => {
+        const { cart } = await commerce.cart.empty()
+
+        setCart(cart)
+    }
+
     useEffect(() => {
         fetchProducts();
         fetchCart();
@@ -32,11 +51,24 @@ const App = () => {
     console.log(cart)
 
     return (
-        <div>
-            <Navbar totalItems={cart.total_items} />
-            <Products products={products} onAddToCart={handleAddToCart} />   
-            <Cart cart={cart} />
-        </div>
+        <Router>
+            <div>
+                <Navbar totalItems={cart.total_items} />
+                <Switch>
+                    <Route exact path='/'>
+                        <Products products={products} onAddToCart={handleAddToCart} />
+                    </Route>
+                    <Route exact path='/cart'>
+                        <Cart 
+                            cart={cart} 
+                            handleUpdateCartQty={handleUpdateCartQty} 
+                            handleRemoveFromCart={handleRemoveFromCart}
+                            handleEmptyCart={handleEmptyCart}
+                        />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     )
 }
 
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 import useStyles from './styles'
 import CartItem from './CartItem/CartItem'
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant='subtitle1'>You have no items in your shpping cart, start adding some!</Typography>
+        <Typography variant='subtitle1'>
+            You have no items in your shpping cart, 
+            <Link to='/' className={classes.link}>start adding some!</Link>
+        </Typography>
     )
     
 
@@ -17,7 +21,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div><CartItem item={item} /></div>
+                        <div><CartItem item={item} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart} /></div>
                     </Grid>
                 ))}
             </Grid>
@@ -26,7 +30,7 @@ const Cart = ({ cart }) => {
                     Subtotal: { cart.subtotal.formatted_with_symbol }
                 </Typography>
                 <div>
-                    <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary'>Empty Cart</Button>
+                    <Button className={classes.emptyButton} size='large' type='button' variant='contained' color='secondary' onClick={handleEmptyCart}>Empty Cart</Button>
                     <Button className={classes.checkoutButton} size='large' type='button' variant='contained' color='primary'>Checkout</Button>
                 </div>
             </div>
@@ -38,7 +42,7 @@ const Cart = ({ cart }) => {
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant='h3' >Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant='h3' gutterBottom >Your Shopping Cart</Typography>
             { !cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
         </Container>
     )
